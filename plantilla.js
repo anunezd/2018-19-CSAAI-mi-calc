9b4802d64016ac10889e53d4fe67541ff38d864c
Alejandro Nunez Doniga <anunezdo@beta03.aulas.gsyc.urjc.es>
2019-02-14 09:56:19 +0100

    Haciendo click

---
+++ plantilla.js
@@ -0,0 +1,15 @@
+//-- Punto de entrada: una vez cargada la página se llama a esta
+//-- funcion: ahí comienza nuestro programa principal
+function main()
+{
+  console.log("Aquí comienza tu código JS...");
+
+  var boton = docucument.getElementById('boton')
+
+  boton.onclik = () => {
+    console.log("Click");
+
+    var display = document.getElementById('display');
+    display.innerHTML = "NUEVO TEXTO"
+  }
+}
