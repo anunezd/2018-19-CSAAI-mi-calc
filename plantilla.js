6d62630ef72f96c7f34688e4f65d6c25343fa6d0
anunezd <alejandro_nd93@hotmail.com>
2019-02-17 16:22:38 +0100

    Todo funcionando menos Ans

--- plantilla.js
+++ plantilla.js
@@ -5,6 +5,7 @@ function main() {
   var operadorb = 0
   var linea = 0
   var resultado = 0
+  var ans = 0
   //var uno = document.getElementById('boton1')
   boton0.onclick = () => {display.innerHTML = display.innerHTML + "0"}
   boton1.onclick = () => {display.innerHTML = display.innerHTML + "1"}
@@ -17,7 +18,9 @@ function main() {
   boton8.onclick = () => {display.innerHTML = display.innerHTML + "8"}
   boton9.onclick = () => {display.innerHTML = display.innerHTML + "9"}
   botonp.onclick = () => {display.innerHTML = display.innerHTML + "."}
-  botonporcentaje.onclick = () => {display.innerHTML = display.innerHTML + "%"}
+  botonelevar.onclick = () => {
+    operadora = display.innerHTML
+    display.innerHTML = display.innerHTML + "^"}
   botonpor.onclick = () => {
     operadora = display.innerHTML
     console.log (operadora)
@@ -34,7 +37,6 @@ function main() {
     operadora = display.innerHTML
     console.log(operadora)
     display.innerHTML = display.innerHTML + "-"}
-  botonans.onclick = () => {display.innerHTML = display.innerHTML + "Ans"}
   botonigual.onclick = () => {
     linea = display.innerHTML
     if (linea.includes("x")) {
@@ -49,11 +51,17 @@ function main() {
     if (linea.includes("-")) {
       operadorb = linea.split("-")[1]
       resultado = parseFloat(operadora) - parseFloat(operadorb)}
+    if (linea.includes("^")) {
+        operadorb = linea.split("^")[1]
+        resultado = parseFloat(operadora) ** parseFloat(operadorb)}
     display2.innerHTML = resultado
     console.log (operadorb)
     console.log (linea)
     console.log (resultado)
   }
-  botondel.onclick = () => {display.innerHTML = "0"}
-  botonac.onclick = () => {display.innerHTML = "0"}
+  botonans.onclick = () => {display.innerHTML = display.innerHTML + "Ans"
+                            ans = parseFloat(resultado);console.log(ans)}
+  botondel.onclick = () => {display.innerHTML = display.innerHTML.slice(0,-1)}
+  botonac.onclick = () => {display.innerHTML = ""
+                           resultadoa = 0; resultadob = 0}
 }
